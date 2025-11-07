a6d7cb44f7ec5fac0c05047b2e357a95bbf4bfb6
somayya <somayya.adusumalli98@gmail.com>
2025-11-07 17:46:43 +0530

    terraform

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -60,55 +60,55 @@ resource "aws_ami_from_instance" "catalogue" {
   )
 }
 
-# resource "aws_lb_target_group" "catalogue" {
-#   name     = "${local.common_name_suffix}-catalogue"
-#   port     = 8080
-#   protocol = "HTTP"
-#   vpc_id   = local.vpc_id
-#   deregistration_delay = 60
+resource "aws_lb_target_group" "catalogue" {
+  name     = "${local.common_name_suffix}-catalogue"
+  port     = 8080
+  protocol = "HTTP"
+  vpc_id   = local.vpc_id
+  deregistration_delay = 60
 
 
-#   health_check {
-#     healthy_threshold = 2
-#     interval = 19
-#     matcher = "200-299"
-#     path = "/health"
-#     port = 8080
-#     protocol = "HTTP"
-#     timeout = 2
-#     unhealthy_threshold = 2
-#   }
-# }
+  health_check {
+    healthy_threshold = 2
+    interval = 19
+    matcher = "200-299"
+    path = "/health"
+    port = 8080
+    protocol = "HTTP"
+    timeout = 2
+    unhealthy_threshold = 2
+  }
+}
 
-# resource "aws_launch_template" "catalogue" {
-#   name = "${local.common_name_suffix}-catalogue"
-#   image_id = "aws_ami_from_instance.catalogue.id"
-
-
-#   instance_initiated_shutdown_behavior = "terminate"
-#   instance_type = "t3.micro"
+resource "aws_launch_template" "catalogue" {
+  name = "${local.common_name_suffix}-catalogue"
+  image_id = "aws_ami_from_instance.catalogue.id"
 
 
-#   vpc_security_group_ids = [local.catalogue_sg_id]
+  instance_initiated_shutdown_behavior = "terminate"
+  instance_type = "t3.micro"
 
-#   tag_specifications {
-#     resource_type = "instance"
 
-#     tags = merge(
-#       local.common_tags,
-#       {
-#         Name = "${local.common_name_suffix}-catalogue"
-#       }
-#     )
-#   }
-#   tag_specifications {
-#     resource_type = "volume"
+  vpc_security_group_ids = [local.catalogue_sg_id]
 
-#     tags = merge(
-#       local.common_tags,
-#       {
-#         Name = "${local.common_name_suffix}-catalogue"
-#       }
-#     )
-#   }
-# }
+  tag_specifications {
+    resource_type = "instance"
+
+    tags = merge(
+      local.common_tags,
+      {
+        Name = "${local.common_name_suffix}-catalogue"
+      }
+    )
+  }
+  tag_specifications {
+    resource_type = "volume"
+
+    tags = merge(
+      local.common_tags,
+      {
+        Name = "${local.common_name_suffix}-catalogue"
+      }
+    )
+  }
+}
