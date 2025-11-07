8417c1ba1a4473469682785bd02d4e0482339400
somayya <somayya.adusumalli98@gmail.com>
2025-11-07 08:44:30 +0530

    terraform

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -48,5 +48,5 @@ resource "aws_ec2_instance_state" "catalogue" {
 resource "aws_ami_from_instance" "catalogue" {
   name               = "${local.common_name_suffix}-catalogue-ami"
   source_instance_id = aws_instance.catalogue.id
-  depends_on = [ aws_ami_from_instance ]
+  depends_on = [ aws_ec2_instance_state.catalogue ]
 }
