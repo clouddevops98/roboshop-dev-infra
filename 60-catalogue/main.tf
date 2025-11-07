014828bffd9d00787a3632dff3a8e65be93b9591
somayya <somayya.adusumalli98@gmail.com>
2025-11-07 17:26:10 +0530

    terraform

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -79,9 +79,10 @@ resource "aws_lb_target_group" "catalogue" {
 
 resource "aws_launch_template" "catalogue" {
   name = "${local.common_name_suffix}-catalogue"
-  image_id = "aws_ami_from_instance.catalogue.id"
+  # image_id = "aws_ami_from_instance.catalogue.id"
+  image_id = "data.aws_ami.joindevops.id"
 
-  # instance_initiated_shutdown_behavior = "terminate"
+  instance_initiated_shutdown_behavior = "terminate"
   instance_type = "t3.micro"
 
 
