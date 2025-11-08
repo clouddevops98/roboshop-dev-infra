5b0823bec5ebbb35ffa2ba20c237394676856246
somayya <somayya.adusumalli98@gmail.com>
2025-11-08 16:06:01 +0530

    terraform

--- 20-bastion/bastion.sh
+++ 20-bastion/bastion.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-sudo growpart /dev/nvme0n1 4 
-sudo lvextend -L +30G /dev/mapper/RootVG-homeVol
-sudo xfs_growfs /home
+growpart /dev/nvme0n1 4 
+lvextend -L +30G /dev/mapper/RootVG-homeVol
+xfs_growfs /home
 
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
